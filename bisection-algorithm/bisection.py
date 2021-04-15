109b5b8e991b1e0fb43bc6951e92c3949edcd261
Huy Nguyen <huynguyen@Huys-MacBook-Air.local>
2021-04-14 23:19:18 -0700

    Add grid to graph

--- bisection-algorithm/bisection.py
+++ bisection-algorithm/bisection.py
@@ -33,6 +33,7 @@ def plot_function(f, plot_interval):
 
     x = np.arange(plot_interval[0], plot_interval[1]+0.1,0.1)
     plt.figure()
+    plt.grid()
     plt.plot(x, f(x))
     plt.show()
 
@@ -53,27 +54,26 @@ def bisection_find(f, interval, num_steps, error_threshold=0.001, plot=False, pl
 
         if f(m) == 0:
             print(f'Found exact solution..')
-            return m
+            return a,b,m
         elif np.abs(f(m)) <= error_threshold:
             print(f'The method converged in {step+1} STEPS with ERROR_THRESHOLD={error_threshold}.')
-            return m
+            return a,b,m
         elif f(m)*f(a) < 0:
             b = m
         else:
             a = m
-
-    return m
+    return a,b,m
 
 
 # INPUT / One variable function
-f = lambda x: x**2 - 4
+f = lambda x: x**3 - 25
 
-root = bisection_find(
+a,b,root = bisection_find(
     f=f,  # One variable function
-    interval=(0,10),  # Inclusive bound interval for estimate root
+    interval=(2,3),  # Inclusive bound interval for estimate root
     num_steps=1000,
-    error_threshold=1e-07,
-    plot=False,  # True will plot the function
+    error_threshold=1e-05,
+    plot=True,  # True will plot the function
     plot_interval=(-10,10))
 
-print(f'ROOT OF THE FUNCTION: {root}')
+print(f'ROOT OF THE FUNCTION: {root} -> INTERVAL ({a},{b})')
